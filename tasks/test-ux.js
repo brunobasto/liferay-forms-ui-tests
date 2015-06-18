c06d023b9ee8c30de52e4aa1f3b27c00d059aef2
Bruno Basto <bruno.basto@liferay.com>
2015-06-18 01:38:03 -0300

    Embeding Selenium

--- tasks/test-ux.js
+++ tasks/test-ux.js
@@ -1,15 +1,30 @@
 'use strict';
 
 var gulp = require('gulp');
+var selenium = require('selenium-standalone');
 
 var runner = require('../tests/ux/runner');
 
 module.exports = function() {
-	gulp.task('test:ux', [], function(done) {
-		runner();
+	gulp.task('test:ux', ['test:ux:start-selenium'], function(done) {
+		runner(function() {
+			done();
+		});
 	});
 
-	gulp.task('test:ux', [], function(done) {
-		runner();
+	gulp.task('test:ux:install-selenium', [], function(done) {
+		selenium.install({
+			version: '2.45.0',
+			baseURL: 'http://selenium-release.storage.googleapis.com',
+			drivers: {
+				version: '2.15',
+				arch: process.arch,
+				baseURL: 'http://chromedriver.storage.googleapis.com'
+			}
+		}, done);
+	});
+
+	gulp.task('test:ux:start-selenium', ['test:ux:install-selenium'], function(done) {
+		selenium.start({}, done);
 	});
 };
--- tests/ux/runner.js
+++ tests/ux/runner.js
@@ -8,7 +8,7 @@ var spawn = require('child_process').spawn,
 	args = [path.join(__dirname, '..', '..', 'node_modules', '.bin', '_mocha')],
 	config = require('./config').config;
 
-module.exports = function() {
+module.exports = function(done) {
 	/**
 	 * execute init script with mocha
 	 * init script handles the rest
@@ -20,6 +20,8 @@ module.exports = function() {
 	});
 
 	proc.on('exit', function(code, signal) {
+		done();
+
 		process.on('exit', function() {
 			if (signal) {
 				process.kill(process.pid, signal);
