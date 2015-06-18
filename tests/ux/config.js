e6cb56a344adfaf4b0eedde59850ac31bcbbf9bd
Bruno Basto <bruno.basto@liferay.com>
2015-06-18 19:10:14 -0300

    Setting base URL to localhost

--- tests/ux/config.js
+++ tests/ux/config.js
@@ -53,7 +53,7 @@ exports.config = {
      * - baseUrl: sets base url for `Given I open the site "/some/url.html"`
      */
     env: {
-        baseUrl: 'http://webdriverjs.christian-bromann.com/'
+        baseUrl: 'http://localhost:8080/'
     },
 
     /**
