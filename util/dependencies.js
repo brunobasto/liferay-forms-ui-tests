29242fa00ed1a93d9b50ad185d8a6457218dae23
Bruno Basto <bruno.basto@liferay.com>
2015-06-17 23:44:18 -0300

    Strict mode

--- util/dependencies.js
+++ util/dependencies.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var path = require('path');
 
 var config = require('../config');
--- util/normalizer.js
+++ util/normalizer.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var config = require('../config');
 var osgi = require('./osgi.js');
 var path = require('path');
