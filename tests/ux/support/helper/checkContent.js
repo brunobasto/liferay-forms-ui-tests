6dddb5b24559cf604b53bc3b132c908c07c78ec3
Bruno Basto <bruno.basto@liferay.com>
2015-06-19 17:12:08 -0300

    Better way of retrieving elements text

--- tests/ux/support/helper/checkContent.js
+++ tests/ux/support/helper/checkContent.js
@@ -3,19 +3,21 @@
  */
 
 module.exports =  function(type, element, falseCase, origText, done) {
+	var assertText = function(err, text) {
+		should.not.exist(err);
 
-    var command = type !== 'inputfield' ? 'getText' : 'getValue';
+		if (falseCase) {
+			origText.should.not.equal(text);
+		}
+		else {
+			origText.should.equal(text);
+		}
+	}
 
-    this.browser[command](element, function(err, text) {
-
-        should.not.exist(err);
-
-        if(falseCase) {
-            origText.should.not.equal(text);
-        } else {
-            origText.should.equal(text);
-        }
-
-    })
-    .call(done);
+	if (type === 'inputfield') {
+		this.browser.getValue(element, assertText).call(done);
+	}
+	else {
+		this.browser.getAttribute(element, 'innerText', assertText).call(done);
+	}
 };
