5e6c2e1371e972fc04ef24ddcb3ad09b019c1038
Bruno Basto <bruno.basto@liferay.com>
2015-06-20 12:59:30 -0300

    Running mocha programatically instead of spawning a process

--- tests/ux/runner.js
+++ tests/ux/runner.js
@@ -1,37 +1,21 @@
-/**
- * wrapper arround mocha cmd
- * figures out options and calls mocha with desired arguments
- */
+var Mocha = require('mocha'),
+	config = require('./config').config,
+    path = require('path');
 
-var spawn = require('child_process').spawn,
-	path = require('path'),
-	args = [path.join(__dirname, '..', '..', 'node_modules', '.bin', '_mocha')],
-	config = require('./config').config;
+module.exports = function(options, done) {
+	var mocha = new Mocha(config.mochaOpts || {});
 
-module.exports = function(done) {
-	/**
-	 * execute init script with mocha
-	 * init script handles the rest
-	 */
-	args.push(path.join(__dirname, 'support', 'init.js'));
+	mocha.addFile(
+		path.join(__dirname, 'support', 'init.js')
+	);
 
-	for(flag in (config.mochaOpts || {})) {
-	    args.push('--' + flag + '=' + config.mochaOpts[flag]);
-	}
+	global.testConfig = options;
 
-	var proc = spawn(process.argv[0], args, {
-		stdio: [0, 1, 2]
-	});
-
-	proc.on('exit', function(code, signal) {
-		done();
-
-		process.on('exit', function() {
-			if (signal) {
-				process.kill(process.pid, signal);
-			} else {
-				process.exit(code);
-			}
+	global.mocha = mocha.run(function(failures){
+		process.on('exit', function () {
+			process.exit(failures);
 		});
+
+		done();
 	});
-}
+};
