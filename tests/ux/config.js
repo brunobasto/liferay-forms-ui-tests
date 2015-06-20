d4f9790f004a9c46d61ed34f6c73c4137b3c7891
Bruno Basto <bruno.basto@liferay.com>
2015-06-20 13:01:46 -0300

    Default capability is to run locally. Use test:ux:saucelabs to run on SauceLabs

--- tests/ux/config.js
+++ tests/ux/config.js
@@ -1,74 +1,69 @@
 exports.config = {
 
-    /**
-     * language of your feature files
-     * options: french, spanish, norwegian, polish, german, russian
-     */
-    language: 'english',
+	/**
+	 * language of your feature files
+	 * options: french, spanish, norwegian, polish, german, russian
+	 */
+	language: 'english',
 
-    /**
-     * set selenium host and port
-     */
-    selenium: {
-        host: 'ondemand.saucelabs.com',
-        port: 80,
-        user: process.env.SAUCE_USERNAME,
-        key: process.env.SAUCE_ACCESS_KEY,
-        recordScreenshots: true
-    },
+	/**
+	 * set selenium host and port
+	 */
+	selenium: {
+		host: 'localhost',
+		port: 4444
+	},
 
-    /**
-     * webdriverio options
-     *
-     * - logLevel: stdout log level
-     *   Options: *verbose* | *silent* | *command* | *data* | *result*
-     *
-     * - coloredLogs: Enables colors for log output
-     *   default: true
-     *
-     * - singleton: Set to true if you always want to reuse the same remote
-     *   default: false
-     *
-     * - waitforTimeout: Default timeout for all waitForXXX commands
-     *   default: 500
-     */
-    options: {
-        logLevel: 'silent',
-    },
+	/**
+	 * webdriverio options
+	 *
+	 * - logLevel: stdout log level
+	 *   Options: *verbose* | *silent* | *command* | *data* | *result*
+	 *
+	 * - coloredLogs: Enables colors for log output
+	 *   default: true
+	 *
+	 * - singleton: Set to true if you always want to reuse the same remote
+	 *   default: false
+	 *
+	 * - waitforTimeout: Default timeout for all waitForXXX commands
+	 *   default: 500
+	 */
+	options: {
+		logLevel: 'silent',
+	},
 
-    /**
-     * desired capabilities
-     */
-    capabilities: {
-        browserName: 'chrome',
-        name: 'Liferay Forms - UX',
-        platform: 'Windows 7',
-        screenResolution: '1600x1200'
-    },
+	/**
+	 * desired capabilities
+	 */
+	capabilities: {
+		browserName: 'chrome',
+		name: 'Liferay Forms - UX'
+	},
 
-    /**
-     * location of feature files
-     */
-    featureFiles: [
-        'tests/ux/features/**/*.feature'
-    ],
+	/**
+	 * location of feature files
+	 */
+	featureFiles: [
+		'tests/ux/features/**/*.feature'
+	],
 
-    /**
-     * environment variables
-     *
-     * - baseUrl: sets base url for `Given I open the site "/some/url.html"`
-     */
-    env: {
-        baseUrl: 'http://localhost:8080/'
-    },
+	/**
+	 * environment variables
+	 *
+	 * - baseUrl: sets base url for `Given I open the site "/some/url.html"`
+	 */
+	env: {
+		baseUrl: 'http://localhost:8080/'
+	},
 
-    /**
-     * mocha options
-     * @see http://mochajs.org/
-     */
-    mochaOpts: {
-        reporter: 'spec',
-        timeout: 120000,
-        require: 'chai'
-    }
+	/**
+	 * mocha options
+	 * @see http://mochajs.org/
+	 */
+	mochaOpts: {
+		reporter: 'spec',
+		timeout: 120000,
+		require: 'chai'
+	}
 };
