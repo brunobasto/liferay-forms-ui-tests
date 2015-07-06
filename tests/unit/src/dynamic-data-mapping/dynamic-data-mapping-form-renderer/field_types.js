bf090c5e3bba0d84099d9e2b19b43447d5ad6baf
Bruno Basto <bruno.basto@liferay.com>
2015-07-06 17:11:39 -0300

    Adding tests for Liferay.DDM.Renderer.FieldTypes

--- tests/unit/src/dynamic-data-mapping/dynamic-data-mapping-form-renderer/field_types.js
+++ tests/unit/src/dynamic-data-mapping/dynamic-data-mapping-form-renderer/field_types.js
@@ -2,25 +2,39 @@
 
 var assert = chai.assert;
 
-describe('DDM Form Field Types Test Suite', function() {
-	this.timeout(5000);
+var getTestData = function(callback) {
+	$.when(
+		$.get('/base/src/dynamic-data-mapping/dynamic-data-mapping-form-renderer/assets/field_types.json')
+	).done(callback);
+};
+
+describe('DDM Renderer Field Types', function() {
+	this.timeout(120000);
 
 	before(function(done) {
+		var test = this;
+
 		AUI().use(
 			'liferay-ddm-form-renderer-field-types',
 			function(A) {
-				assert.ok(Liferay.DDM.Renderer.FieldTypes);
+				getTestData(function(fieldTypes) {
+					test.fieldTypes = fieldTypes;
 
-				done();
+					done();
+				});
 			}
 		);
 	});
 
-	it('should register Field Types', function(done) {
-		var A = AUI(),
+	it('should register all Field Types', function(done) {
+		var test = this,
 			FieldTypes = Liferay.DDM.Renderer.FieldTypes;
 
-		assert.equal(true, true, 'Unimplemented test.');
+		FieldTypes.register(test.fieldTypes);
+
+		_.each(FieldTypes.getAll(), function(fieldType, index) {
+			assert.equal(fieldType.get('name'), test.fieldTypes[index].name);
+		});
 
 		done();
 	});
