9f4f7c78d144f7ac6396ed405c520b365cf0430f
Bruno Basto <bruno.basto@liferay.com>
2015-07-27 18:22:43 -0300

    Adding tests for DOM events

---
+++ tests/unit/src/dynamic-data-mapping/dynamic-data-mapping-form-renderer/field_events_support.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var assert = chai.assert;
+
+var getTestData = function(callback) {
+	$.when(
+		$.get('/base/src/dynamic-data-mapping/dynamic-data-mapping-form-renderer/assets/field_types.json')
+	).done(callback);
+};
+
+describe('DDM Renderer Field Events Support', function() {
+	this.timeout(120000);
+
+	before(function(done) {
+		AUI().use(
+			'node-event-simulate',
+			'liferay-ddm-form-renderer-field',
+			'liferay-ddm-form-renderer-field-repetition',
+			function(A) {
+				getTestData(function(fieldTypes) {
+					Liferay.DDM.Renderer.FieldTypes.register(fieldTypes);
+
+					done();
+				});
+			}
+		);
+	});
+
+	it('should fire the "valueChanged" event when input node changes', function(done) {
+		var field = new Liferay.DDM.Renderer.Field({
+			type: 'text'
+		});
+
+		var callback = sinon.spy();
+
+		field.on('valueChanged', callback);
+
+		field.getInputNode().simulate('change');
+
+		assert.isTrue(callback.calledOnce);
+
+		field.destroy();
+
+		done();
+	});
+
+	it('should still fire the "valueChanged" event when container changes', function(done) {
+		var A = AUI(),
+			field = new Liferay.DDM.Renderer.Field({
+				type: 'text'
+			});
+
+		var callback = sinon.spy();
+
+		field.on('valueChanged', callback);
+
+		field.set('container', A.Node.create('<div></div>'));
+
+		field.render();
+
+		field.getInputNode().simulate('change');
+
+		assert.isTrue(callback.calledOnce);
+
+		field.destroy();
+
+		done();
+	});
+});
