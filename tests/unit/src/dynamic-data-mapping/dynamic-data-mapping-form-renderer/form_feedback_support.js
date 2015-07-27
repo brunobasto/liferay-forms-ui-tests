4d6ff273e72e52bf541a5d79eb51ba1b5aee6ba1
Bruno Basto <bruno.basto@liferay.com>
2015-07-27 18:23:14 -0300

    Adding tests for Form Feedback support

---
+++ tests/unit/src/dynamic-data-mapping/dynamic-data-mapping-form-renderer/form_feedback_support.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var assert = chai.assert;
+
+describe('DDM Renderer Form Feedback Support', function() {
+	before(function(done) {
+		AUI().use(
+			'liferay-ddm-form-renderer',
+			function(A) {
+				done();
+			}
+		);
+	});
+
+	it('should show a loading spinner on the form submit button after calling .showLoadingFeedback()', function(done) {
+		var A = AUI(),
+			formNode = A.Node.create('<form name="myFeedbackForm" id="myFeedbackForm" action=""><button type="submit" /></form>');
+
+		formNode.appendTo(document.body);
+
+		var form = new Liferay.DDM.Renderer.Form({
+			container: formNode
+		});
+
+		var submitButton = formNode.one('[type="submit"]');
+
+		form.showLoadingFeedback();
+
+		assert.ok(submitButton.one('.icon-spinner'))
+
+		form.destroy();
+
+		formNode.remove();
+
+		done();
+	});
+});
